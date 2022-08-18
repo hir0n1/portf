f45d938624a8ff0a45ea3d3a4d0eeb923beee965
Hiron <jion.ru.08@mail.ru>
2022-08-19 02:36:38 +0300

    works social

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => { 
+
+    const onScrollHeader = () => { 
+  
+      const header = document.querySelector('.header') 
+  
+      let prevScroll = window.pageYOffset
+      let currentScroll 
+  
+      window.addEventListener('scroll', () => {  
+
+        currentScroll = window.pageYOffset  
+
+        const headerHidden = () => header.classList.contains('header_out')   
+
+        if (currentScroll > prevScroll && !headerHidden()) { 
+          header.classList.add('header_out') 
+        }
+        if (currentScroll < prevScroll && headerHidden()) { 
+          header.classList.remove('header_out')
+        }  
+        prevScroll = currentScroll  
+      })
+    }
+    onScrollHeader() 
+  });

344244cbfc51984fab6697b169ca7d7a8e6a4569
Hiron <jion.ru.08@mail.ru>
2023-04-02 12:10:33 +0300

    changes1

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,51 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => { 
 
-const header = document.querySelector('.header') 
+// const header = document.querySelector('.header') 
     
-	const onScrollHeader = () => { 
+// 	const onScrollHeader = () => { 
   
-      let prevScroll = window.pageYOffset
-      let currentScroll 
+//       let prevScroll = window.pageYOffset
+//       let currentScroll 
   
-      window.addEventListener('scroll', () => {  
+//       window.addEventListener('scroll', () => {  
 
-        currentScroll = window.pageYOffset  
+//         currentScroll = window.pageYOffset  
 
-        const headerHidden = () => header.classList.contains('header_out')   
+//         const headerHidden = () => header.classList.contains('header_out')   
 
-        if (currentScroll > prevScroll && !headerHidden()) { 
-          header.classList.add('header_out') 
-        }
-        if (currentScroll < prevScroll && headerHidden()) { 
-          header.classList.remove('header_out')
-        }  
-        prevScroll = currentScroll  
-      })
-    }
-    onScrollHeader() 
+//         if (currentScroll > prevScroll && !headerHidden()) { 
+//           header.classList.add('header_out') 
+//         }
+//         if (currentScroll < prevScroll && headerHidden()) { 
+//           header.classList.remove('header_out')
+//         }  
+//         prevScroll = currentScroll  
+//       })
+//     }
+//     onScrollHeader() 
 
 
 /// Модальные окна 
 
 	const popup = document.querySelectorAll('.popup');
 	const popupTest = document.querySelector('.js-popupTest');
-  const popupFitness = document.querySelector('.js-popupTest');
+  	const popupFitness = document.querySelector('.js-popupFitness');
 	const iconFitness = document.querySelector('.js-iconFitness');
-  const iconTest = document.querySelector('.js-test');
+ 	const iconTest = document.querySelector('.js-test');
 	const popupClose = document.querySelectorAll('.js-popupClose');
 
 
 	iconTest.addEventListener("click", function() {
 		popupTest.classList.add('popup_active');
-		header.classList.add('header_out');
+		document.body.style.overflow = 'hidden';
 	});	
-  iconFitness.addEventListener("click", function() {
+	
+  	iconFitness.addEventListener("click", function() {
 		popupFitness.classList.add('popup_active');
-		header.classList.add('header_out');
+		document.body.style.overflow = 'hidden';
 	});	
 
 	popupClose.forEach((item) => {
     item.addEventListener('click', function(){
+		document.body.style.overflow = '';
         popup.forEach((i) => {
           i.classList.remove('popup_active');
         });
--- src/js/lang.js
+++ src/js/lang.js
@@ -5,15 +5,17 @@ const arrLang = {
         'menuWorks': 'Works',
         'menuSocial': 'Social',
         'mainName': 'Ivan Sheyko',
-        'mainDesc': 'Beginner Front-end developer 25 years old, Donetsk',
+        'mainDesc': 'Front-end developer 25 years old, Donetsk',
         'aboutTitle': 'About',
-        'aboutText': "<p> My programming career was started by spending 70% of my free time at the computer. At university I came across such languages as: Assembly, C, C++.</p> <br> <p>The final choice fell on Front-end development, because development of the interface and functions seem to be the most interesting among other areas.</p> <br> <p>Graduated from Udemy's Web Developer course. Good knowledge of <strong>HTML/CSS</strong>. <br> Main skills: <strong>adaptive layout, preprocessors, BEM, Flex-box, Grid, Gulp, Git</strong>.</p> <br> <p><strong>JavaScript и JQuery</strong>. I studied <strong>native JS</strong> and try to consolidate all the knowledge gained in practice. </p><br> <p>The level of English is <strong>Intermediate</strong> or B1.</p> <br> <p>I consider my main skill to be easy to learn and the ability to use Google.</p> <br> <p>I understand that there is still a lot to be explored, but the ambitions are huge. In a year I'm going to become a Senior developer. But it is not exactly.</p>",
+        'aboutText': "<p>Graduated from Udemy's Web Developer course. Good knowledge of <strong>HTML/CSS</strong>. <br> Main skills: <strong>adaptive layout, preprocessors, BEM, Flex-box, Grid, Gulp, Git</strong>.</p> <br> <p><strong>JavaScript и JQuery</strong>. I studied <strong>native JS</strong> and try to consolidate all the knowledge gained in practice. </p><br> <p>The level of English is <strong>Intermediate</strong> or B1.</p> <br> <p>I consider my main skill to be easy to learn and the ability to use Google.</p>",
         'worksTitle': '<p>Works</p>',
         'socialTitle': '<p>Contact me</p>',
         'popupTestTitle': '<p>Responsive landing page layout</p>',
         'popupTestDesc': '<p>The task was to make an adaptive landing page using CSS as much as possible</p> <p>In my work I used JQuery, SCSS, FLEX, GRID</p>',
         'popupBtnCode': 'View code',
-        'popupBtnWeb': 'View website'
+        'popupBtnWeb': 'View website',
+        'popupFitnessTitle': '<p>Responsive landing page layout</p>',
+        'popupFitnessDesc': '<p>The task was to develop an interactive builder that would allow users to create burgers by selecting ingredients and optional add-ons.</p> <p>I used JS, SСSS, FLEX, GRID, Git</p>',
         
     },
     'ru': {
@@ -22,30 +24,47 @@ const arrLang = {
         'menuWorks': 'Работы',
         'menuSocial': 'Контакты',
         'mainName': 'Иван Шейко',
-        'mainDesc': 'Начинающий Front-end разработчик 25 лет, Донецк',
+        'mainDesc': 'Front-end разработчик 25 лет, Донецк',
         'aboutTitle': 'Обо мне',
-        'aboutText': '<p>Свой путь в программировании начинал с проведения за компьютером 70% свободного времени. В университете сталкивался с такими языками как: Assembly, C, C++.</p> <br> <p>Окончательный выбор пал на Front-end разработку, т.к. разработка интерфейса и функций кажутся наиболее интересными среди остальных направлений.</p> <br> <p>Окончил курс Udemy “Веб-разработчик”. Хорошо знаю <strong>HTML/CSS</strong>. <br> Ключевые навыки: <strong>адаптивная верстка, препроцессоры, БЭМ, Flex-box, Grid, Gulp, Git</strong>.</p> <br> <p><strong>JavaScript и JQuery</strong>. Изучил <strong>нативный JS</strong> и стараюсь закрепить все полученные знания на практике. </p><br> <p>Английским владею на уровне <strong>Intermediate</strong> или B1.</p> <br> <p>Своим главным навыком считаю легкообучаемость и умение пользоваться гуглом.</p> <br> <p>Понимаю, что много еще предстоит изучить, но амбиции огромные. Через годик собираюсь стать Senior разработчиком. Но это не точно.</p>',
+        'aboutText': '<p>Окончил курс Udemy “Веб-разработчик”. Хорошо знаю <strong>HTML/CSS</strong>. <br> Ключевые навыки: <strong>адаптивная верстка, препроцессоры, БЭМ, Flex-box, Grid, Gulp, Git</strong>.</p> <br> <p><strong>JavaScript и JQuery</strong>. Изучил <strong>нативный JS</strong> и стараюсь закрепить все полученные знания на практике. </p><br> <p>Английским владею на уровне <strong>Intermediate</strong> или B1.</p> <br> <p>Своим главным навыком считаю легкообучаемость и умение пользоваться гуглом.</p>',
         'worksTitle': '<p>Работы</p>',
         'socialTitle': '<p>Свяжитесь со мной</p>',
         'popupTestTitle': '<p>Адаптивная верстка лендинга</p>',
         'popupTestDesc': '<p>Задачей было сверстать адаптивно лендинг максимально используя CSS</p><p>В работе использовал JQuery, SCSS, FLEX, GRID</p>',
         'popupBtnCode': 'Посмотреть код',
-        'popupBtnWeb': 'Перейти на сайт'
+        'popupBtnWeb': 'Перейти на сайт',
+        'popupFitnessTitle': '<p>Адаптивная верстка лендинга</p>',
+        'popupFitnessDesc': '<p>Задачей было разработать интерактивный конструктор, который позволил бы пользователям создавать бургеры, выбирая ингредиенты и опциональные добавки. </p>  <p>в работе использовал JS, SСSS, FLEX, GRID, Git</p>',
     },
 };
 
 
-$('.main__btn').each(function() {
-    $(this).click(function() {
+// $('.main__btn').each(function() {
+//     $(this).click(function() {
 
-        $('.main__btn').removeClass('main__btn_black')
-        $(this).addClass('main__btn_black')
+//         // $('.main__btn').removeClass('main__btn_black')
+//         // $(this).addClass('main__btn_black')
         
-        let lang = $(this).attr('id');
+//         let lang = $(this).attr('id');
         
-        $('.lang').each(function() {
-            $(this).html(arrLang[lang][$(this).attr('key')]);
+//         $('.lang').each(function() {
+//             $(this).html(arrLang[lang][$(this).attr('key')]);
+//         });
+//     });
+// });
+
+const langChange = document.querySelectorAll('.main__btn');
+const lang = document.querySelectorAll('.lang');
+
+langChange.forEach((itemLang) => {
+    itemLang.addEventListener('click', () => {
+        langChange.forEach((itemLang) => {
+            itemLang.classList.remove('main__btn_black')
+        });
+        itemLang.classList.add('main__btn_black')
+
+        lang.forEach((item) => {
+            item.innerHTML = arrLang[itemLang.getAttribute('id')][item.getAttribute('key')]
         });
     });
-});
-
+})
